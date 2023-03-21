bd7b9d822d861c969a28b05546c11b99278c1f19
helena stefansdottir <hel>
2023-03-21 10:56:12 +0000

    Spurning 3 e

--- src/main/java/is/hi/hbv202g/ass9/compositeLeafObservedByComposite/composite/Observable.java
+++ src/main/java/is/hi/hbv202g/ass9/compositeLeafObservedByComposite/composite/Observable.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Observable {
-    
+
     private List<Observer> observers = new ArrayList<Observer>();
 
     public void add(Observer observer) {
-
+        observers.add(observer);
     }
 
     public void remove(Observer observer) {
+        observers.remove(observer);
 
     }
 
@@ -20,11 +21,16 @@ public class Observable {
     }
 
     public void lastObservedResult(int number) {
+        for (Observer observer : observers) {
+            observer.lastObservedResult(number);
+        }
 
     }
 
     public void setLastObservedResult(int number) {
-
+        for (Observer observer : observers) {
+            observer.lastObservedResult(number);
+        }
     }
 
     public int getResult() {

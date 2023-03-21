83e7403b409551b8ce84628aba2446e4ef54d653
helena stefansdottir <hel>
2023-03-21 10:34:29 +0000

    Spurning 3 d

--- src/main/java/is/hi/hbv202g/ass9/NumberLeaf.java
+++ src/main/java/is/hi/hbv202g/ass9/NumberLeaf.java
@@ -2,8 +2,9 @@ package is.hi.hbv202g.ass9;
 
 
 import is.hi.hbv202g.ass9.composite.MathExpression;
+import is.hi.hbv202g.ass9.compositeLeafObservedByComposite.composite.Observable;
 
-public class NumberLeaf implements MathExpression {
+public class NumberLeaf extends Observable implements MathExpression {
     private int number;
 
     public NumberLeaf(int number) {
@@ -11,6 +12,11 @@ public class NumberLeaf implements MathExpression {
     }
 
     public int getResult() {
+        return number;
+    }
+
+    public int SetValue(int number) {
+        this.number = number;
         return number;
     }
 
